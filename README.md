7dc8ecb6b4434b6bc5ede8c21284702e842fcfd7
Ludovic Jozeau <unifai@protonmail.com>
2022-10-29 12:22:18 +0200

    Use Prism Launcher

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # How to apply the fix
 
-1. Be sure to launch [polymc](https://polymc.org/download/linux/) at least once
+1. Be sure to launch [PrismLauncher](https://github.com/PrismLauncher/PrismLauncher/releases) at least once
 2. Download [applyfix.desktop](https://github.com/FederAndInk/glfw_aqm2_fix/releases/download/1.0.0/applyfix.desktop)
 3. Launch applyfix.desktop by (double) clicking on it like you launch an application
--- install.sh
+++ install.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! "$(command -v polymc)" ] && [ ! -x "/var/lib/flatpak/app/org.polymc.PolyMC/current/active/files/bin/polymc" ]; then
-  echo "No PolyMC installation found, please install PolyMC and rerun this script"
+if [ ! "$(command -v prismlauncher)" ] && { [ ! "$(command -v flatpak)" ] || ! flatpak list | grep "Prism Launcher" >/dev/null; }; then
+  echo "No PrismLauncher installation found, please install PrismLauncher and rerun this script"
   exit 1
 fi
 
-function add_fix_to_polymc() {
-  local polymc_path=$1
-  if [ -d "$polymc_path" ]; then
-    if [ ! -f "$polymc_path/polymc.cfg" ]; then
-      printf '\033[1m%s\033[0m\n' "$polymc_path/polymc.cfg not found, try launching PolyMC first"
+function add_fix_to_prismlauncher() {
+  local prismlauncher_path=$1
+  if [ -d "$prismlauncher_path" ]; then
+    if [ ! -f "$prismlauncher_path/prismlauncher.cfg" ]; then
+      printf '\033[1m%s\033[0m\n' "$prismlauncher_path/prismlauncher.cfg not found, try launching PrismLauncher first"
       return 1
     else
-      printf '\033[1m%s\033[0m\n' "Adding glfw fix to PolyMC \"$polymc_path\""
-      cd "$polymc_path"
+      printf '\033[1m%s\033[0m\n' "Adding glfw fix to PrismLauncher \"$prismlauncher_path\""
+      cd "$prismlauncher_path"
 
-      cat >"$polymc_path/glfw_fix.sh" <<EOF
+      cat >"$prismlauncher_path/glfw_fix.sh" <<EOF
 #!/usr/bin/env bash
 cd "\$1"
 mkdir natives
 cp "\$(dirname "\$0")/libglfw.so" natives/
 EOF
 
-      wget https://github.com/FederAndInk/glfw_aqm2_fix/raw/main/libglfw.so -O "$polymc_path/libglfw.so"
-      chmod +x "$polymc_path/libglfw.so"
+      wget https://github.com/FederAndInk/glfw_aqm2_fix/raw/main/libglfw.so -O "$prismlauncher_path/libglfw.so"
+      chmod +x "$prismlauncher_path/libglfw.so"
 
-      mv polymc.cfg "polymc.cfg-$(date '+%s').bak"
-      sed -e "s|^PreLaunchCommand=.*$|PreLaunchCommand=bash \"$polymc_path/glfw_fix.sh\" \"\$INST_DIR\"|" -e 's/^UseNativeGLFW=false$/UseNativeGLFW=true/' polymc.cfg.bak >polymc.cfg
-      if ! grep 'UseNativeGLFW=true' polymc.cfg; then
-        echo 'UseNativeGLFW=true' >>polymc.cfg
+      mv prismlauncher.cfg "prismlauncher.cfg-$(date '+%s').bak"
+      sed -e "s|^PreLaunchCommand=.*$|PreLaunchCommand=bash \"$prismlauncher_path/glfw_fix.sh\" \"\$INST_DIR\"|" -e 's/^UseNativeGLFW=false$/UseNativeGLFW=true/' prismlauncher.cfg.bak >prismlauncher.cfg
+      if ! grep 'UseNativeGLFW=true' prismlauncher.cfg; then
+        echo 'UseNativeGLFW=true' >>prismlauncher.cfg
       fi
-      if ! grep -q "PreLaunchCommand=bash \"$polymc_path/glfw_fix.sh\" \"\$INST_DIR\"" polymc.cfg; then
-        echo "PreLaunchCommand=bash \"$polymc_path/glfw_fix.sh\" \"\$INST_DIR\"" >>polymc.cfg
+      if ! grep -q "PreLaunchCommand=bash \"$prismlauncher_path/glfw_fix.sh\" \"\$INST_DIR\"" prismlauncher.cfg; then
+        echo "PreLaunchCommand=bash \"$prismlauncher_path/glfw_fix.sh\" \"\$INST_DIR\"" >>prismlauncher.cfg
       fi
     fi
   fi
@@ -40,15 +40,19 @@ EOF
 
 one_fixed=false
 
-if add_fix_to_polymc "$HOME/.var/app/org.polymc.PolyMC/data/PolyMC"; then
+if add_fix_to_prismlauncher "$HOME/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher"; then
   one_fixed=true
 fi
 
-if add_fix_to_polymc "$HOME/.local/share/PolyMC"; then
+if add_fix_to_prismlauncher "$HOME/.local/share/PrismLauncher"; then
+  one_fixed=true
+fi
+
+if add_fix_to_prismlauncher "$HOME/.local/share/PolyMC"; then
   one_fixed=true
 fi
 
 if ! $one_fixed; then
-  printf '\033[1;31mNo fix applied! Try running polymc and rerun this\033[0m\n'
+  printf '\033[1;31mNo fix applied! Try running prismlauncher and rerun this\033[0m\n'
   exit 1
 fi
